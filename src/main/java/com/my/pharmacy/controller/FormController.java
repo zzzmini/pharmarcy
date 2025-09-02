11f9066657051cc3e8f930c1ab6414c889f152d6
zzzmini <sm01079200887@gmail.com>
2025-09-02 15:53:58 +0900

    화면 출력 처리

--- src/main/java/com/my/pharmacy/controller/FormController.java
+++ src/main/java/com/my/pharmacy/controller/FormController.java
@@ -1,10 +1,30 @@
 package com.my.pharmacy.controller;
 
+import com.my.pharmacy.dto.DocumentDto;
+import com.my.pharmacy.dto.InputDto;
+import com.my.pharmacy.dto.KakaoApiResponseDto;
+import com.my.pharmacy.dto.OutputDto;
+import com.my.pharmacy.service.KakaoAddressSearchService;
+import com.my.pharmacy.service.KakaoCategorySearchService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class FormController {
+    private final KakaoAddressSearchService kakaoAddressSearchService;
+    private final KakaoCategorySearchService kakaoCategorySearchService;
+
+    public FormController(KakaoAddressSearchService kakaoAddressSearchService,
+                            KakaoCategorySearchService kakaoCategorySearchService) {
+        this.kakaoAddressSearchService = kakaoAddressSearchService;
+        this.kakaoCategorySearchService = kakaoCategorySearchService;
+    }
+
+
     @GetMapping("/")
     public String mainForm() {
         return "main";
@@ -12,6 +32,32 @@ public class FormController {
 
     @GetMapping("/output")
     public String outputForm() {
+        return "output";
+    }
+
+    @PostMapping("/search")
+    public String searchAddress(InputDto dto, Model model) {
+        // 주소 검색 API 호출
+        KakaoApiResponseDto kakaoApiResponseDto = kakaoAddressSearchService
+                .requestAddressSearch(dto.getAddress());
+        // 카테고리 서비스 호출을 위해서 documents 배열에서 0번째 것 만 뺀다.
+        DocumentDto documentDto = kakaoApiResponseDto
+                .getDocumentList()
+                .get(0);
+
+        // 카테고리 검색
+        double radius = 1000;
+        KakaoApiResponseDto recommendationDto =
+                kakaoCategorySearchService.resultCategorySearch(
+                        documentDto.getLatitude(),
+                        documentDto.getLongitude(),
+                        radius);
+        // recommendationiDto에서 3개만 추출
+        List<OutputDto> outputDtoList =
+                kakaoCategorySearchService.makeOutputDto(
+                        recommendationDto.getDocumentList());
+        System.out.println(outputDtoList);
+        model.addAttribute("outputList", outputDtoList);
         return "output";
     }
 }
---
+++ src/main/java/com/my/pharmacy/dto/InputDto.java
@@ -0,0 +1,10 @@
+package com.my.pharmacy.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class InputDto {
+    private String address;
+}
