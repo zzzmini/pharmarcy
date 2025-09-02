2cf2f4da37004b97830968692930b93712c1d661
zzzmini <sm01079200887@gmail.com>
2025-09-02 14:40:00 +0900

    KAKAO_API 카테고리 검색 처리

--- src/test/java/com/my/pharmacy/service/KakaoAddressSearchServiceTest.java
+++ src/test/java/com/my/pharmacy/service/KakaoAddressSearchServiceTest.java
@@ -12,6 +12,9 @@ class KakaoAddressSearchServiceTest {
     @Autowired
     KakaoAddressSearchService kakaoAddressSearchService;
 
+    @Autowired
+    KakaoCategorySearchService kakaoCategorySearchService;
+
     @Test
     @DisplayName("API KEY Test")
     void test() {
@@ -24,6 +27,19 @@ class KakaoAddressSearchServiceTest {
         KakaoApiResponseDto dto = new KakaoApiResponseDto();
         dto = kakaoAddressSearchService
                 .requestAddressSearch("강남대로 405");
+        System.out.println(dto);
+    }
+
+    @Test
+    @DisplayName("Search Category Test")
+    void categoryTest() {
+        KakaoApiResponseDto dto = new KakaoApiResponseDto();
+        double x = 127.0263154489116;
+        double y = 37.49855885145178;
+        double radius = 1000;
+
+        dto = kakaoCategorySearchService
+                .resultCategorySearch(y, x, radius);
         System.out.println(dto);
     }
 }
